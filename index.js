9a7848f2bb5dbe5954edf54c20a2e3916447a517
Philip Bruland <philipbruland@gmail.com>
2019-07-23 11:55:10 +0100

    Issue #9: Setup Security
    - Install helmet
    - Install express-rate-limit

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 /* INCLUDES ****************************** */
 const express = require('express');
 const session = require('express-session');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const passport = require('passport');
 const flash = require('flash');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const passportConfig = require('./Middleware/Passport/config');
+/* **************************************** */
+
+/* CONSTANTS ****************************** */
+const RATELIMITHITS = 10;
+const RATELIMITINTERVAL = 1 * 60 * 1000; // 1 Min
 /* **************************************** */
 
 /* INCLUDE CONFIGS ************************ */
@@ -22,10 +29,30 @@ const app = express();
 app.use(bodyParser());
 /* **************************************** */
 
+/* SECURITY SETUP ************************* */
+// Potentiall could run only on production
+
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+const limiter = rateLimit({
+  windowMs: RATELIMITINTERVAL,
+  max: RATELIMITHITS,
+});
+
+// only apply to requests that begin with /api/
+app.use(limiter);
+app.use(helmet());
+/* **************************************** */
+
 /* SESSION SETUP ************************** */
 const sesh = {
   secret: 'Oh hi there', // Replaced with proper setup in production
-  cookie: { secure: false },
+  cookie: {
+    secure: false,
+    maxAge: 60000,
+  },
 };
 
 if (app.get('env') === 'production') {
